9d665d4d723f29e3be0c04fb62520a34b3bcc044
Connie Ho <Ho.Connie10@gmail.com>
2017-09-30 21:43:06 -0700

    Added Text

--- index.vr.js
+++ index.vr.js
@@ -7,27 +7,30 @@ import {
   View,
   Model,
   PointLight,
+  Text,
+  Box,
+  Image
 } from 'react-vr';
 
 export default class HelloWorld extends React.Component {
   render() {
     return (
       <View>
-        <PointLight
-          style={{color: 'white', transform: [{translate: [0, 400, 700]}]}}
-        />
-        <Pano source={asset('chess-world.jpg')} />
-        <Model
-          style={{
-            transform: [
-              {translate: [0, -5, -20]},
-              {rotateY: -30},
-              {rotateX: -40},
-            ],
-          }}
-          source={{obj: asset('cube2.obj'), mtl: asset('cube2.mtl')}}
-          lit
-        />
+      <PointLight
+      style={{color: 'white', transform: [{translate: [0, 400, 700]}]}}
+      />
+      <Pano source={asset('chess-world.jpg')} />
+      <Text
+      style={{
+        fontSize: 5,
+        layoutOrigin: [0.5, 0.5],
+        transform: [
+          {translate: [0, -5, -20]}
+        ],
+        color: '#fffff',
+      }}>
+      hello
+      </Text>
       </View>
     );
   }

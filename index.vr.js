22ce21e5e3095c52494cd8b1ac7888ace9dad50b
Connie Ho <Ho.Connie10@gmail.com>
2017-09-30 23:07:23 -0700

    Read and Display JSON

--- index.vr.js
+++ index.vr.js
@@ -9,10 +9,26 @@ import {
   PointLight,
   Text,
   Box,
-  Image
+  Image, 
+  VrButton,
 } from 'react-vr';
 
 export default class HelloWorld extends React.Component {
+constructor(props){
+  super(props);
+  this.state = {
+    isToggleOn: true,
+    result:JSON.parse('{"id":229294491,"url":"/229294491/cs-questions-flash-cards/","title":"CS Questions","created_by":"vincentw_","term_count":5,"created_date":1506819253,"modified_date":1506820304,"published_date":1506820304,"has_images":false,"subjects":[],"visibility":"public","editable":"only_me","has_access":true,"can_edit":false,"description":"","lang_terms":"en","lang_definitions":"en","password_use":0,"password_edit":0,"access_type":2,"creator_id":69076717,"creator":{"username":"vincentw_","account_type":"free","profile_image":"https://quizlet.com/a/i/animals/18.cGWP.jpg","id":69076717},"class_ids":[],"terms":[{"id":7566697266,"term":"When talking about computer memory, what does the acronym ROM stand for?","definition":"Read-only memory","image":null,"rank":0},{"id":7566700352,"term":"How many bits are in a byte?","definition":"8","image":null,"rank":1},{"id":7566712668,"term":"What does HTML stand for?","definition":"Hypertext Markup Language","image":null,"rank":2},{"id":7566746488,"term":"The binary system uses powers of","definition":"2","image":null,"rank":3},{"id":7566770044,"term":"Java and JavaScript are the same. True or False?","definition":"False","image":null,"rank":4}]}'),
+    index:0,
+  }
+  this.handleClick = this.handleClick.bind(this);
+}
+
+handleClick() {
+  var oldIndex = index;
+  this.state.index = oldIndex+1;
+}
+
   render() {
     return (
       <View>
@@ -20,20 +36,30 @@ export default class HelloWorld extends React.Component {
       style={{color: 'white', transform: [{translate: [0, 400, 700]}]}}
       />
       <Pano source={asset('chess-world.jpg')} />
+      <View
+      style={{
+        flexDirection:'row',
+        width: 20,
+        flexGrow: 1,
+      }}>
       <Text
       style={{
-        fontSize: 5,
+        fontSize: 2,
         layoutOrigin: [0.5, 0.5],
         transform: [
-          {translate: [0, -5, -20]}
+          {translate: [0, 0, -20]}
         ],
-        color: '#fffff',
+        color: 'white',
+        flex: 1, 
+        flexWrap: 'wrap'
       }}>
-      hello
+      {this.state.result.terms[this.state.index].term}
       </Text>
+      </View>
       </View>
     );
   }
 }
+
 
 AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
